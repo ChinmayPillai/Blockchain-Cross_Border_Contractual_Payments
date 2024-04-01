e58a96e54f34b77e06b3612369a16f73142441f6
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-04-01 09:39:04 +0530

    Add Routing

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
 )
