8ed6b2ab688758123d32965d3e290560e8f9b198
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-04-02 19:20:03 +0530

    Add Home page

---
+++ Client/src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import {Typography} from '@mui/material';
+
+export default function Home() {
+    return (
+        <div>
+            <Typography variant="h1" align="center">Welcome to the BlockPe!</Typography>
+            <Typography variant="body1" align="center"> Use BlockPe to create and manage contractual payments between international parties. </Typography>
+        </div>
+    );
+}
