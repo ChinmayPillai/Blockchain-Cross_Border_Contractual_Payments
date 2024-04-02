2005432ab4605bea8a21764ff67dc804f93a7176
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-04-02 19:20:49 +0530

    Add RequestedContracts page

---
+++ Client/src/components/RequestedContracts/RequestedContracts.jsx
@@ -0,0 +1,9 @@
+import {Typography} from '@mui/material'
+export default function RequestedContracts() {
+    return (
+        <div>
+            <Typography variant="h1" align="center">Requested Contracts</Typography>
+            <Typography variant="body1" align="center"> Here you can view and manage your requested contracts. </Typography>
+        </div>
+    )
+}
