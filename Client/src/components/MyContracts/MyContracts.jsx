72a69c2da0603f9e1fb3c95218bb582d87dec65b
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-04-02 19:20:33 +0530

    Add MyContracts page

---
+++ Client/src/components/MyContracts/MyContracts.jsx
@@ -0,0 +1,9 @@
+import {Typography} from '@mui/material'
+export default function MyContracts() {
+    return (
+        <div>
+            <Typography variant="h1" align="center">My Contracts</Typography>
+            <Typography variant="body1" align="center"> Here you can create and view your active contracts. </Typography>
+        </div>
+    )
+}
