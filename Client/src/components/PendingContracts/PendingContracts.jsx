c9440228079aae48123a81a4f2e544f702899043
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-04-02 19:21:04 +0530

    Add PendingContracts page

---
+++ Client/src/components/PendingContracts/PendingContracts.jsx
@@ -0,0 +1,9 @@
+import {Typography} from '@mui/material'
+export default function PendingContracts() {
+    return (
+        <div>
+            <Typography variant="h1" align="center">Pending Contracts</Typography>
+            <Typography variant="body1" align="center"> Here you can view and manage your pending contracts. </Typography>
+        </div>
+    )
+}
