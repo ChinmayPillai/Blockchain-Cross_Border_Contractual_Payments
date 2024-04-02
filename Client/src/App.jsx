28a92fc56b1f01004c15ab312187652177c4cbcc
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-04-02 19:18:30 +0530

    Add Routing to all pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,12 @@ import {Route, Routes, BrowserRouter} from 'react-router-dom'
 import './App.css'
 import Test from './components/Test'
 import Navbar from './components/NavBar/NavBar'
+import Home from './components/Home/Home'
+import MyContracts from './components/MyContracts/MyContracts'
+import RequestedContracts from './components/RequestedContracts/RequestedContracts'
+import PendingContracts from './components/PendingContracts/PendingContracts'
+import Dashboard from './components/Dashboard/Dashboard'
+import Login from './components/Login/Login'
 
 function App() {
 
@@ -9,7 +15,12 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navbar />}>
-          <Route  path="/" element={<Test />} />
+          <Route  index element={<Home />} />
+          <Route  path="/contracts" element={<MyContracts />} />
+          <Route  path="/requests" element={<RequestedContracts />} />
+          <Route  path="/pending" element={<PendingContracts />} />
+          <Route  path="/dashboard" element={<Dashboard />} />
+          <Route  path="/login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
