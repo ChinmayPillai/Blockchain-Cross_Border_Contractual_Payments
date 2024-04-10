3524e5ddc0bab5dc7ddfad7509dc5a45fe5748a7
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-04-10 15:43:00 +0530

    Update apiUrls

--- Client/src/Util/apiUrls.jsx
+++ Client/src/Util/apiUrls.jsx
@@ -4,3 +4,5 @@ export const ContractsUrlBase = 'http://localhost:3000/contracts/';
 export const RequestedContractsUrlBase = 'http://localhost:3000/requestedContracts/';
 export const PendingContractsUrlBase = 'http://localhost:3000/pendingContracts/';
 export const NewContractUrl = 'http://localhost:3000/createContractAsset';
+export const acceptByContractorURL = 'http://localhost:3000/acceptByContractor';
+export const acceptByManagerURL = 'http://localhost:3000/acceptByManager';
